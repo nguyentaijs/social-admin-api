27db56072380170cdad1f19391d0fb0e5ea8c928
Tafi <terrance@DESKTOP-DM0SQOI>
2024-09-21 00:19:15 +0700

    Remove localhost in config

--- src/main/java/io/github/nguyentaijs/socialadminapi/config/WebConfig.java
+++ src/main/java/io/github/nguyentaijs/socialadminapi/config/WebConfig.java
@@ -11,7 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://195.35.20.32:9399")
-                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials (optional)
