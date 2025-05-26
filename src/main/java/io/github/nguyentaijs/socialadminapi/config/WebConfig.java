62cc2094d2153cec51f41ca25a4a0c3782de76cf
Tafi <nguyentaijs@gmail.com>
2025-05-26 15:11:20 +0700

    Add allowed origin

--- src/main/java/io/github/nguyentaijs/socialadminapi/config/WebConfig.java
+++ src/main/java/io/github/nguyentaijs/socialadminapi/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://195.35.20.32:9399")
+                .allowedOrigins(
+                        "http://195.35.20.32:9399",
+                        "http://195.35.20.32:9400"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow credentials (optional)
